88da93fe6a29f29a07dacd5093ae4370d4b65345
teddybear0430 <teddybear0430@hotmail.com>
2024-01-24 00:53:21 +0900

    first commit

---
+++ src/components/UserForm/UserForm.stories.tsx
@@ -0,0 +1,72 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { UserForm } from "./UserForm";
+import { expect, within, userEvent } from "@storybook/test";
+
+const meta = {
+  component: UserForm,
+} satisfies Meta<typeof UserForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// nameの入力
+export const InputName: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const nameInput = canvas.getByRole("textbox", {
+      name: "Name",
+    });
+    await userEvent.type(nameInput, "example-name", {
+      delay: 100,
+    });
+  },
+};
+
+// ageの入力
+export const InputAge: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const ageInput = canvas.getByRole("spinbutton", {
+      name: "Age",
+    });
+    await userEvent.type(ageInput, "20", {
+      delay: 100,
+    });
+  },
+};
+
+// emailの入力
+export const InputEmail: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const emailInput = canvas.getByRole("textbox", {
+      name: "Email",
+    });
+    await userEvent.type(emailInput, "example-email@email.com", {
+      delay: 100,
+    });
+  },
+};
+
+export const InputAll: Story = {
+  play: async (ctx) => {
+    await InputName.play?.(ctx);
+    await InputAge.play?.(ctx);
+    await InputEmail.play?.(ctx);
+
+    // submit
+    const canvas = within(ctx.canvasElement);
+    const submitButton = canvas.getByRole("button", {
+      name: "Submit",
+    });
+    await userEvent.click(submitButton);
+
+    const listItems = canvas.getAllByRole("listitem");
+    expect(listItems[0]).toHaveTextContent("Name: example-name");
+    expect(listItems[1]).toHaveTextContent("Age: 20");
+    expect(listItems[2]).toHaveTextContent("Email: example-email");
+  },
+};
