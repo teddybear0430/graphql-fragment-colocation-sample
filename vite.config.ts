e74d1cf5e53cf77810cd47d5d6d28224b3e15d09
teddybear0430 <teddybear0430@hotmail.com>
2024-01-27 17:36:41 +0900

    fix: 凡ミス。

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    TanStackRouterVite(),
-  ],
-})
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    // ↓ 追記
+    alias: {
+      "~/": `${__dirname}/src/` // path.join(__dirname, "src/") でも可
+    }
+    // alias: [
+    //   { find: "~/", replacement: `${__dirname}/src/` }
+    // ],
+  }
+});
