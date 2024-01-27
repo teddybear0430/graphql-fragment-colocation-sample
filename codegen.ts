d8891b4a1fed84d06f75fa4974c979d4003e40a3
teddybear0430 <teddybear0430@hotmail.com>
2024-01-27 14:26:47 +0900

    update: 一旦不要な設定消してみた。

--- codegen.ts
+++ codegen.ts
@@ -1,16 +1,13 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "https://graphql.anilist.co",
+  // クライアントサイド用のコードを生成するために必要
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {
       preset: "client",
       plugins: []
-    },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
     }
   }
 };
